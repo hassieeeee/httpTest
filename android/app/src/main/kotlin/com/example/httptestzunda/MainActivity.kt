1856a47c4f22fd2c82b176a77be322ccf1b225af
hassieeeee <tackt_84@au.com>
2023-06-26 21:14:34 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/httptestzunda/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.httptestzunda
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:geolocator/geolocator.dart';
+
+import 'dart:developer';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+
+Future<String> get() async {
+  const domain = '';//ここに'http://0.0.0.0:0000'の形でサーバのアドレスとポート番号を記述する
+  var url = Uri.parse(domain);
+  var response = await http.get(url);
+  Map<String, dynamic> map = jsonDecode(response.body);
+  String message = map['message'];
+  return message;
+}
+
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  String text = '受け取ったメッセージがここに表示されます';
+
+
+  Future<void> _incrementCounter() async {
+    String result = await get();
+    setState(() {
+      text = result;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              text,
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
